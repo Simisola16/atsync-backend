10d2d1b2cea56dc5c8bf4073ed9af09ba1c1934a
Muhayad olamilekan <olamilekanmuhayad@yahoo.com>
2026-05-15 19:16:50 +0200

    fix: resolve cors configuration

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const authRoutes = require('./routes/auth');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
